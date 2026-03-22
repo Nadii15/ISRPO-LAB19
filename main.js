b356bec2f50815dce845bb67ef40747bc32bf30c
okolodk <grisha.zhivot@bk.ru>
2026-03-22 15:31:21 +0300

    Step 8: objects in JavaScript

--- main.js
+++ main.js
@@ -189,19 +189,106 @@
 // console.log(fruits.includes("Яблоко"));
 // console.log(fruits.includes("Манго"));
 
-let cities = ["Москва", "Санкт-Петербург", "Казань", "Новосибирск", "Екатеринбург"];
-let searchCity = "Казань";
-if (cities.includes(searchCity)) {
-    let index = cities.indexOf(searchCity);
-    console.log(`Город "${searchCity}" найден! Индекс: ${index}`);
-} else {
-    console.log(`Город "${searchCity}" не найден в списке.`);
-}
-let searchCity2 = "Сочи";
-if (cities.includes(searchCity2)) {
-    let index = cities.indexOf(searchCity2);
-    console.log(`Город "${searchCity2}" найден! Индекс: ${index}`);
-} else {
-    console.log(`Город "${searchCity2}" не найден в списке.`);
-}
-console.log("Список городов:", cities);
+// let cities = ["Москва", "Санкт-Петербург", "Казань", "Новосибирск", "Екатеринбург"];
+// let searchCity = "Казань";
+// if (cities.includes(searchCity)) {
+//     let index = cities.indexOf(searchCity);
+//     console.log(`Город "${searchCity}" найден! Индекс: ${index}`);
+// } else {
+//     console.log(`Город "${searchCity}" не найден в списке.`);
+// }
+// let searchCity2 = "Сочи";
+// if (cities.includes(searchCity2)) {
+//     let index = cities.indexOf(searchCity2);
+//     console.log(`Город "${searchCity2}" найден! Индекс: ${index}`);
+// } else {
+//     console.log(`Город "${searchCity2}" не найден в списке.`);
+// }
+// console.log("Список городов:", cities);
+
+// let user2 = {
+//     name:"Ivan",
+//     age: 20,
+//     isStudent: true,
+// };
+// console.log(user2);
+
+// console.log(user2.name);
+// console.log(user2.age);
+// console.log(user2["name"]);
+
+// let book = {
+//     title: "Мастер и Маргарита",
+//     author: "Михаил Булгаков",
+//     year: 1967
+// };
+
+// console.log("Название:", book.title);
+// console.log("Автор:", book.author);
+// console.log("Год издания:", book.year);
+// console.log("Объект book:", book);
+// book.year = 1966; 
+// console.log("\n--- После изменения ---");
+// console.log("Новый год издания:", book.year);
+// console.log("Объект book после изменений:", book);
+
+// user2.age = 30;
+// user2.name = "Кирилл";
+// delete user2.isStudent;
+// console.log(user2);
+
+// let user2 = {
+//     name:"Ivan",
+//     age: 20,
+//     isStudent: true,
+//     sayHello: function(){
+//         console.log(`hello, my name is ${name}`);
+//     },
+// };
+// user2.sayHello();
+
+
+// let car = {
+//     brand: "Toyota",
+//     year: 2020,
+//     getInfo: function() {
+//         console.log(`Автомобиль: ${this.brand}, Год выпуска: ${this.year}`);
+//     }
+// };
+// car.getInfo();
+// car.year = 2021;
+// console.log("После изменения года:");
+// car.getInfo();
+
+// for (let key in user2){
+//     console.log(key +": " + user2[key]);
+// }
+
+
+// let product = {
+//     title: "Ноутбук",
+//     price: 75000,
+//     category: "Электроника",
+//     inStock: true,
+//     rating: 4.8
+// };
+// console.log("Свойства объекта product:");
+// for (let key in product) {
+//     console.log(`${key}: ${product[key]}`);
+// }
+// product.discount = 10;
+// console.log("После добавления свойства 'discount':");
+// for (let key in product) {
+//     console.log(`${key}: ${product[key]}`);
+// }
+
+let student = {
+    name:"Григорий",
+    skills: ["HTML","CSS","JS"],
+    address:{
+        city: "Волжский",
+        street: "Пускина",
+    },
+};
+console.log(student.skills[0]);
+console.log(student.address.city);
